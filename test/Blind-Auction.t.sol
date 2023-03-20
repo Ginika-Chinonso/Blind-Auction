448451b37df35620822b6cc66408724d315d6daf
Ginika Chinonso <ginika.chinonso.francis@gmail.com>
2023-03-20 21:45:49 +0100

    refactor: Fully functional test

--- test/Blind-Auction.t.sol
+++ test/Blind-Auction.t.sol
@@ -12,51 +12,60 @@ import "../src/IBlindAuction.sol";
 
 contract BlindAuctionTest is Test {
     BlindAuctionFactory public factory;
-    BlindAuction public auction;
     W3BNFT public nftContract;
 
     function setUp() public {
+        vm.startPrank(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
         factory = new BlindAuctionFactory();
-        auction = new BlindAuction(3600, address(this));
         nftContract = new W3BNFT("Web 3 Bridge", "W3B");
         nftContract.mint(1);
+        vm.stopPrank();
     }
 
     function test_AuctionFactory() public {
+        vm.prank(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
         factory.createBlindAuction(3600 minutes);
     }
 
 
     function test_createAuction() public {
+        vm.startPrank(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
         address sampleAuction = factory.createBlindAuction(3600 minutes);
         IERC721(nftContract).approve(sampleAuction, 1);
         IBlindAuction(sampleAuction).createAuction(address(nftContract), 1);
+        vm.stopPrank();
     }
 
 
     function test_cancelAuction() public{
+        vm.startPrank(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
         address sampleAuction = factory.createBlindAuction(3600);
         IERC721(nftContract).approve(sampleAuction, 1);
         IBlindAuction(sampleAuction).createAuction(address(nftContract), 1);
         IBlindAuction(sampleAuction).cancelAuction();
+        vm.stopPrank();
     }
 
 
     function testFail_commitAfterCancelAuctionBeforeAuctionEnd() public{
+        vm.startPrank(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
         address sampleAuction = factory.createBlindAuction(3600);
         IERC721(nftContract).approve(sampleAuction, 1);
         IBlindAuction(sampleAuction).createAuction(address(nftContract), 1);
         IBlindAuction(sampleAuction).cancelAuction();
         IBlindAuction(sampleAuction).CommitBid(20, "money");
+        vm.stopPrank();
     }
 
 
     function test_CancelIfNoBidorAddressZeroWinner() public{
+        vm.startPrank(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
         address sampleAuction = factory.createBlindAuction(3600);
         IERC721(nftContract).approve(sampleAuction, 1);
         IBlindAuction(sampleAuction).createAuction(address(nftContract), 1);
         vm.warp(block.timestamp + 3600 minutes);
         IBlindAuction(sampleAuction).getWinner();
+        vm.stopPrank();
     }
 
 
@@ -79,9 +88,11 @@ contract BlindAuctionTest is Test {
 
 
      function test_FullAuction() public {
+        vm.startPrank(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
         address sampleAuction = factory.createBlindAuction(3600);
         IERC721(nftContract).approve(sampleAuction, 1);
         IBlindAuction(sampleAuction).createAuction(address(nftContract), 1);
+        vm.stopPrank();
         IBlindAuction(sampleAuction).CommitBid(10 ether, "moneyman");
         vm.prank(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
         IBlindAuction(sampleAuction).CommitBid(20 ether, "moniman");
@@ -89,10 +100,12 @@ contract BlindAuctionTest is Test {
         IBlindAuction(sampleAuction).RevealBid(10 ether, "moneyman");
         vm.prank(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
         IBlindAuction(sampleAuction).RevealBid(20 ether, "moniman");
+        vm.prank(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
         IBlindAuction(sampleAuction).getWinner();
         vm.deal(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, 10000 ether);
         vm.prank(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
         IBlindAuction(sampleAuction).claimItem{value: 20 ether}();
+        vm.prank(0x70997970C51812dc3A010C7d01b50e0d17dc79C8);
         IBlindAuction(sampleAuction).withdrawFunds();
     }
 
